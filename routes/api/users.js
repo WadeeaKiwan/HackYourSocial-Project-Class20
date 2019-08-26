aeae997c44faffdaf49774d1e53ac4befd12f5f8
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2019-08-26 15:58:46 +0200

    Resend email confirmation route backend

--- routes/api/users.js
+++ routes/api/users.js
@@ -61,11 +61,6 @@ router.post(
         },
       }
 
-      // jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
-      //   if (err) throw err;
-      //   res.json({ token });
-      // });
-
       const token = await jwt.sign(payload, config.get('jwtSecret'), { expiresIn: '12h' })
 
       // Check if not token
@@ -79,8 +74,8 @@ router.post(
             <br/><br/>
             Thanks for your registration!
             <br/><br/>
-            Please verify your account by clicking the following link:
-            <a href="http://localhost:3000/verify/${token}">http://localhost:3000/verify</a>
+            Please verify your account by clicking: 
+            <a href="http://localhost:3000/verify/${token}">Here</a>
             <br/><br/>
             Thanks, Hack Your Social Team
             `
@@ -103,7 +98,7 @@ router.post(
 
 // @route    POST api/users/verify/:token
 // @desc     Email Confirmation
-// @access   Private
+// @access   Public
 router.post('/verify/:token', async (req, res) => {
   const { token } = req.params
 
@@ -113,7 +108,7 @@ router.post('/verify/:token', async (req, res) => {
     let user = await User.findById({ _id: verifyToken.user.id }).select('-password')
 
     if (!user) {
-      return res.status(400).json({ errors: [{ msg: 'User not found' }] })
+      return res.status(400).json({ errors: [{ msg: 'Activation link has expired!' }] })
     }
 
     user.active = true
@@ -146,5 +141,68 @@ router.post('/verify/:token', async (req, res) => {
     res.status(500).send('Server error')
   }
 })
+
+// @route    PUT api/users/verify/resend
+// @desc     Resend Email Confirmation
+// @access   Public
+router.put(
+  '/verify/resend',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+  ], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ errors: [{ msg: 'User not found' }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      const token = await jwt.sign(payload, config.get('jwtSecret'), { expiresIn: '1h' });
+
+      // Check if not token
+      if (!token) {
+        return res.status(401).json({ msg: 'No token, authorization denied' });
+      }
+
+      // Email body
+      const html = `
+            Hi ${user.name},
+            <br/><br/>
+            Please verify your account by clicking: 
+            <a href="http://localhost:3000/verify/${token}">Here</a>
+            <br/><br/>
+            Thanks, Hack Your Social Team
+            `;
+
+      // Send the email
+      await sendEmail(
+        '"HackYourSocial Activation 👻" <activate@hackyoursocial.com>',
+        email,
+        'Please verify your account',
+        html,
+      );
+
+      res.json({ msg: 'Please, visit your email to confirm your account' });
+    } catch (err) {
+      console.error(err.message);
+      if (err.kind === 'ObjectId') {
+        return res.status(404).json({ msg: 'User not found' });
+      }
+      res.status(500).json({ msg: err.message });
+    }
+  });
 
 module.exports = router
