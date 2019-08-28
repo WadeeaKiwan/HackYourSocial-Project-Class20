8d5ce4bffbccf7ff97414ff4216d457812761bb8
RamazanInce <mathince1.62@gmail.com>
2019-08-28 21:48:40 +0200

    edit comment

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -3,36 +3,38 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Moment from 'react-moment';
-import { deleteComment } from '../../actions/post';
+import { deleteComment, setEditPost } from '../../actions/post';
 
 const CommentItem = ({
   postId,
   comment: { _id, text, name, avatar, user, date },
   auth,
-  deleteComment
+  setEditPost,
+  deleteComment,
 }) => (
   <div className='post bg-white p-1 my-1'>
     <div>
       <Link to={`/profile/${user}`}>
         <img className='round-img' src={avatar} alt='' />
-        <h4>{name}</h4>
-      </Link>
-    </div>
+        <h4> {name} </h4>{' '}
+      </Link>{' '}
+    </div>{' '}
     <div>
-      <p className='my-1'>{text}</p>
+      <p className='my-1'> {text} </p>{' '}
       <p className='post-date'>
-        Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
-      </p>
+        Posted on <Moment format='YYYY/MM/DD'> {date} </Moment>{' '}
+      </p>{' '}
       {!auth.loading && user === auth.user._id && (
-        <button
-          onClick={() => deleteComment(postId, _id)}
-          type='button'
-          className='btn btn-danger'
-        >
-          <i className='fas fa-times' />
+        <button onClick={() => setEditPost(_id)} type='button' className='btn btn-light'>
+          <i className='fas fa-pencil-alt' />
         </button>
       )}
-    </div>
+      {!auth.loading && user === auth.user._id && (
+        <button onClick={() => deleteComment(postId, _id)} type='button' className='btn btn-danger'>
+          <i className='fas fa-times' />
+        </button>
+      )}{' '}
+    </div>{' '}
   </div>
 );
 
@@ -40,14 +42,18 @@ CommentItem.propTypes = {
   postId: PropTypes.string.isRequired,
   comment: PropTypes.object.isRequired,
   auth: PropTypes.object.isRequired,
-  deleteComment: PropTypes.func.isRequired
+  deleteComment: PropTypes.func.isRequired,
+  setEditPost: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
-  auth: state.auth
+  auth: state.auth,
 });
 
 export default connect(
   mapStateToProps,
-  { deleteComment }
+  {
+    deleteComment,
+    setEditPost,
+  },
 )(CommentItem);
