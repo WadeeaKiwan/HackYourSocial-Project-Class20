e6cf142cc6616762544657a54268e20dcd6969d0
eneserdek <enesf.erdek@gmail.com>
2019-08-30 11:54:44 +0300

    Social Media Login backend with three arguments

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,8 +5,69 @@ const auth = require('../../middleware/auth');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator/check');
+let generator = require('generate-password');
 
 const User = require('../../models/User');
+
+// @route    POST api/auth/registerWithSocialMedia
+// @desc     Login SocialMedia
+// @access   Public
+router.post(
+  '/registerWithSocialMedia',
+
+  async (request, response) => {
+    // come from the body
+
+    const { name, email, avatar } = request.body;
+
+    // generate password
+    let password = generator.generate({
+      length: 10,
+      numbers: true,
+      uppercase: true,
+      symbols: true,
+    });
+
+    try {
+      // check if the user exists
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        // create instance
+        user = new User({
+          name,
+          email,
+          avatar,
+          password,
+        });
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+      }
+      // Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw error;
+          response.status(200).send({ token });
+        },
+      );
+    } catch (error) {
+      console.log(error);
+      response.status(404).json({ error });
+    }
+  },
+);
 
 // @route    GET api/auth
 // @desc     Test route
@@ -42,13 +103,17 @@ router.post(
       let user = await User.findOne({ email });
 
       if (!user) {
-        return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
       }
 
       const isMatch = await bcrypt.compare(password, user.password);
 
       if (!isMatch) {
-        return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
       }
 
       const payload = {
@@ -57,10 +122,15 @@ router.post(
         },
       };
 
-      jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
-        if (err) throw err;
-        res.json({ token });
-      });
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        },
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
