ac51c5ef593f2febf7ba9c6a2c4939dd63fd98c8
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2019-08-30 13:36:27 +0200

    Revert "cleaning"
    This reverts commit 432542452fa07e8e6930e4db2a409138e21409d1.

--- client/src/components/auth/AccountVerification.js
+++ client/src/components/auth/AccountVerification.js
@@ -23,10 +23,9 @@ const AccountVerification = ({ verifyAccount, resendEmail, match, auth: { loadin
     setEmailResend('')
   }
 
-  return loading || msg === null ? (
-    <Spinner />
-  ) : (
-      verify ? (
+  return loading || msg === null ? (<Spinner />) : (
+    <Fragment>
+      {verify && (
         <Fragment>
           <h1 className="large text-primary">{msg}</h1>
           <p className="lead my-1">Your are now an official member of Hack Your Social network </p>
@@ -34,40 +33,43 @@ const AccountVerification = ({ verifyAccount, resendEmail, match, auth: { loadin
             Sign in
           </Link>
         </Fragment>
-      ) : (
-          <Fragment>
-            <h1 className="large text-danger">{msg}</h1>
+      )}
 
-            {!displayResend && (
-              <button onClick={() => toggleResend(!displayResend)} className="btn btn-primary">Resend Confirmation Link</button>
-            )}
+      {!verify && (
+        <Fragment>
+          <h1 className="large text-danger">{msg}</h1>
 
-            {displayResend && (
-              <form
-                className='form my-1'
-                onSubmit={e => resendEmailSubmit(e)}
-              >
-                <input
-                  type="email"
-                  placeholder="Email Address"
-                  name="emailResend"
-                  value={emailResend}
-                  onChange={e => onChange(e)}
-                  required
-                />
-                <input
-                  type="submit"
-                  className="btn btn-primary my-1"
-                  value="Resend"
-                />
-                <button onClick={() => toggleResend(false)} className='btn btn-light my-1'>
-                  Cancel
-            </button>
-              </form>
-            )}
-          </Fragment>
-        )
-    )
+          {!displayResend && (
+            <button onClick={() => toggleResend(!displayResend)} className="btn btn-primary">Resend Confirmation Link</button>
+          )}
+
+          {displayResend && (
+            <form
+              className='form my-1'
+              onSubmit={e => resendEmailSubmit(e)}
+            >
+              <input
+                type="email"
+                placeholder="Email Address"
+                name="emailResend"
+                value={emailResend}
+                onChange={e => onChange(e)}
+                required
+              />
+              <input
+                type="submit"
+                className="btn btn-primary my-1"
+                value="Resend"
+              />
+              <button onClick={() => toggleResend(false)} className='btn btn-light my-1'>
+                Cancel
+              </button>
+            </form>
+          )}
+        </Fragment>
+      )}
+    </Fragment>
+  )
 }
 
 AccountVerification.propTypes = {
